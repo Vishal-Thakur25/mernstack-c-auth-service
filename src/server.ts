e316c5eae19e1057ab2af452f78d6d41d18fafac
Vishal Singh <Vishalsingh25269@gmail.com>
2025-04-09 13:43:32 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 // function Welcome(name: string) {
 //     return name
 // }
-
+import app from './app'
 import { Config } from './config'
 
 // Welcome('Singh')
 
 // If we want to use env variable in our function
-console.log(Config.PORT)
+// console.log(Config.PORT)
+
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => console.log(`Server listen on PORT ${PORT}`))
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+startServer() // This will start the server
