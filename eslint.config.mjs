dee231b6c93f024945fad36e960a37ff1cff4362
Vishal Singh <Vishalsingh25269@gmail.com>
2025-04-10 12:13:38 +0530

    add jest setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,12 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+        ],
     },
 
     eslint.configs.recommended,
