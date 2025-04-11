56ae53af1792a1dc6a85653be53f7f720077b97e
Vishal Singh <Vishalsingh25269@gmail.com>
2025-04-11 11:49:13 +0530

    write first test

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
-import express, { NextFunction, Request, Response } from 'express'
-import logger from './config/logger'
-import { HttpError } from 'http-errors'
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
+import authRouter from './routes/auth';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
     // const err = createHttpError(401, 'You can not access this route')
     // next(err)
-    res.send('Welcome to express app')
-})
+    res.send('Welcome to express app');
+});
+
+app.use('/auth', authRouter);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message)
+    logger.error(err.message);
 
-    const statusCode = err.statusCode || 500
+    const statusCode = err.statusCode || 500;
 
     res.status(statusCode).json({
         errors: [
@@ -25,7 +29,7 @@ app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
                 location: '',
             },
         ],
-    })
-})
+    });
+});
 
-export default app
+export default app;
---
+++ tests/users/users.spec.ts
@@ -0,0 +1,68 @@
+import app from '../../src/app';
+import request from 'supertest';
+
+describe('POST /auth/register', () => {
+    describe('Given all Fields', () => {
+        it('should return 201 status code', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: 'Vishal',
+                lastName: 'Singh',
+                email: 'vishal@gmail.com',
+                password: '123456',
+            };
+
+            // Act
+
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert
+
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('should return user data in json format', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                fisrtName: 'Vishal',
+                lastName: 'Singh',
+                email: 'vishal@gmail.com',
+                password: '123456',
+            };
+
+            // Act
+            const res = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert
+            expect(
+                (res.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'));
+        });
+
+        it('should persist user data in database', async () => {
+            // AAA
+            // Arrange
+            const userData = {
+                firstName: 'Vishal',
+                lastName: 'Singh',
+                email: 'vishal@gmail.com',
+                password: '123456',
+            };
+
+            // Act
+            const res = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert
+        });
+    });
+
+    describe('Fieds are missing', () => {});
+});
